30c95d1619bf6c7c3f40f6a809fd75622e115eed
Drandany <drandany@gmail.com>
2021-08-24 21:47:06 +0200

    Movement Sway

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -6,30 +6,45 @@ public class GunController : MonoBehaviour
 {
     public Gun gun;
     public PlayerLook playerLook;
+    public FPMovement playerMovement;
 
     public float SwayAmount;
     public float SwaySmoothing;
     public float SwayResetSmoothing;
+    public float MovementSwaySmoothing;
 
-    public float swayClampX;
-    public float swayClampY;
+    public Vector2 MovementSway;
+    public Vector2 swayClamp;
 
     private Vector3 newWeaponRotation;
     private Vector3 newWeaponRotationVelocity;
     private Vector3 targetWeaponRotation;
     private Vector3 targetWeaponRotationVelocity;
 
+    private Vector3 newWeaponMovementRotation;
+    private Vector3 newWeaponMovementRotationVelocity;
+    private Vector3 targetWeaponMovementRotation;
+    private Vector3 targetWeaponMovementRotationVelocity;
+
     private void Update()
     {
         targetWeaponRotation.y += SwayAmount * playerLook.mouseX * Time.deltaTime;
         targetWeaponRotation.x += SwayAmount * playerLook.mouseY * Time.deltaTime;
 
-        targetWeaponRotation.x = Mathf.Clamp(targetWeaponRotation.x, -swayClampX, swayClampX);
-        targetWeaponRotation.y = Mathf.Clamp(targetWeaponRotation.y, -swayClampY, swayClampY);
+        targetWeaponRotation.x = Mathf.Clamp(targetWeaponRotation.x, -swayClamp.x, swayClamp.x);
+        targetWeaponRotation.y = Mathf.Clamp(targetWeaponRotation.y, -swayClamp.y, swayClamp.y);
+        targetWeaponRotation.z = targetWeaponRotation.y;
 
         targetWeaponRotation = Vector3.SmoothDamp(targetWeaponRotation, Vector3.zero, ref targetWeaponRotationVelocity, SwayResetSmoothing);
         newWeaponRotation = Vector3.SmoothDamp(newWeaponRotation, targetWeaponRotation, ref newWeaponRotationVelocity, SwaySmoothing);
 
-        gun.transform.localRotation = Quaternion.Euler(newWeaponRotation);
+
+        targetWeaponMovementRotation.z = MovementSway.x * Input.GetAxis("Horizontal");
+        targetWeaponMovementRotation.x = MovementSway.y * Input.GetAxis("Vertical");
+
+        targetWeaponMovementRotation = Vector3.SmoothDamp(targetWeaponMovementRotation, Vector3.zero, ref targetWeaponMovementRotationVelocity, MovementSwaySmoothing);
+        newWeaponMovementRotation = Vector3.SmoothDamp(newWeaponMovementRotation, targetWeaponMovementRotation, ref newWeaponMovementRotationVelocity, MovementSwaySmoothing);
+
+        gun.transform.localRotation = Quaternion.Euler(newWeaponRotation + newWeaponMovementRotation);
     }
 }
