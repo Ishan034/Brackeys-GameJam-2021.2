a9a884245ac3434745aeb17d47658447a4fdcf7a
Ishan034 <76897280+Ishan034@users.noreply.github.com>
2021-08-24 21:28:16 +0530

    Now the enemy will damage the player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,9 +6,19 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private float damage;
+    [SerializeField] Vector3 offset;
 
     private void Update()
     {
-        GetComponent<NavMeshAgent>().SetDestination(player.position);
+        GetComponent<NavMeshAgent>().SetDestination(player.position + offset);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+            if (other.CompareTag("Player"))
+            {
+                player.GetComponent<PlayerTakeDamage>().DamageHealth(damage);
+            }
     }
 }
