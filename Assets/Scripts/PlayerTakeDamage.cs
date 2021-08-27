a86dc9a32f81e1f2f45458d92616b2dfb1e974ca
Ishan034 <76897280+Ishan034@users.noreply.github.com>
2021-08-27 12:12:22 +0530

    Revert "Added some ui"
    This reverts commit c82fa1e52a430df8a488b992a88e32c5c9d91547.

--- Assets/Scripts/PlayerTakeDamage.cs
+++ Assets/Scripts/PlayerTakeDamage.cs
@@ -1,20 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
 
 public class PlayerTakeDamage : MonoBehaviour
 {
-    public float health = 100f;
-    public Slider healthBar;
-
-    public TMP_Text playerHealthCounter;
-
-    private void Update()
-    {
-        playerHealthCounter.text = health.ToString();
-        healthBar.value = health;
-    }
+    public float health;
 
     public void DamageHealth(float amount)
     {
