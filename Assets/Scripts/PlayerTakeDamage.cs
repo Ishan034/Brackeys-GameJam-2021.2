8017929da6d5692ade9af856736415573ab95fee
Ishan034 <76897280+Ishan034@users.noreply.github.com>
2021-08-27 19:51:06 +0530

    Added a healthBar

--- Assets/Scripts/PlayerTakeDamage.cs
+++ Assets/Scripts/PlayerTakeDamage.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerTakeDamage : MonoBehaviour
 {
     public float health;
+
+    public Slider healthBar;
+    public TMP_Text healthCounter;
+
+    private void Update()
+    {
+        healthBar.value = health;
+        healthCounter.text = health.ToString();
+    }
 
     public void DamageHealth(float amount)
     {
